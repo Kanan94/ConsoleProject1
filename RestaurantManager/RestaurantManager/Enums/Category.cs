c249418c11b8b97adf27dbc8411bb65bbb1d8ad9
Kanan Madatov <57615204+Kanan94@users.noreply.github.com>
2020-12-29 09:59:23 +0400

    Menu Item class

---
+++ RestaurantManager/RestaurantManager/Enums/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantManager.Enums
+{
+    public enum Category
+    {
+        Soup,
+        MainCourse,
+        Drink,
+        Dessert
+
+    }
+}
---
+++ RestaurantManager/RestaurantManager/Models/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestaurantManager.Enums;
+
+namespace RestaurantManager.Models
+{
+   public class MenuItem
+    {
+        public string No { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public  Category Category { get; set; }
+
+        public MenuItem (string no, string name, int price, Category category)
+        {
+            this.No = no;
+            this.Name = name;
+            this.Price = price;
+            this.Category = category;
+        }
+    }
+}
